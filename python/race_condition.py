0acd9b224d9f2ef15f296aefaced8c3807e379c6
Lewis Kabui <lewisemm@users.noreply.github.com>
2021-03-06 09:34:22 +0300

    Use lock to prevent race condition

--- python/race_condition.py
+++ python/race_condition.py
@@ -1,22 +1,29 @@
 import logging
 import time
+import threading
 import concurrent.futures
 
 class FakeDatabase:
     def __init__(self):
         self.value = 0
+        self._lock = threading.Lock()
     
     def update(self, name):
         logging.info(f'Thread {name} starting update.')
-        local_copy = self.value
-        local_copy += 1
-        time.sleep(0.1)
-        self.value = local_copy
+        logging.debug(f'Thread {name} about to lock.')
+        with self._lock:
+            logging.debug(f'Thread {name} has lock.')
+            local_copy = self.value
+            local_copy += 1
+            time.sleep(0.1)
+            self.value = local_copy
+            logging.debug(f'Thread {name} about to release lock.')
+        logging.debug(f'Thread {name} after release.')
         logging.info(f'Thread {name} finishing update.')
 
 if __name__ == '__main__':
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
+    logging.basicConfig(format=format, level=logging.DEBUG,
                         datefmt="%H:%M:%S")
     database = FakeDatabase()
     logging.info(f'Testing update. Starting value is {database.value}.')
